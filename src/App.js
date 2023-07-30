ee50b85a963f53905cf3b7a7f6cc5b9d733a8a3c
Nikhil Choudhary <nickchoudharyc72@gmail.com>
2023-07-30 23:50:40 +0530

    lesson 12 Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
 import Navbar from './Component/Navbar';
 import TextForm from './Component/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#10123a'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
     <>
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3" >
-        <TextForm heading="Enter your text to analyze below" />
+        <TextForm heading="Enter your text to analyze below" mode={mode}/>
       </div>
     </>
   );
--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -16,27 +16,32 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
+    const handleAltClick = () =>{       
+        setText(text.split("").map(item => item === item.toUpperCase()? item.toLowerCase(): item.toUpperCase()).join(''))
+    }
+
     const [text, setText] = useState("Enter text here")
     // text="Change Text"  wrong way to change state
     // setText("new text");
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color:props.mode==='dark'?'white':'black'}} >
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'#212529':'white', color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 
             </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleAltClick}>Convert to AlternatingCase</button>
         </div>
 
-        <div className="classname my-3">
+        <div className="classname my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} Characters</p>
             <p>{0.008*text.split(" ").length} Minutes Read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox above"}</p>
         </div>
         </>
     )
