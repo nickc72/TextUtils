e6c847ba326f4de0b9e51e60029dfbe98f2526f8
Nikhil Choudhary <nickchoudharyc72@gmail.com>
2023-07-31 16:00:37 +0530

    lesson 13 Alert

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './Component/Navbar';
 import TextForm from './Component/TextForm';
+import Alert from './Component/Alert';
 
 function App() {
   const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
 
-  const toggleMode = () =>{
-    if(mode==='light'){
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor='#10123a'
+      document.body.style.backgroundColor = '#10123a'
+      showAlert('Dark mode has been enabled','success')
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor='white'
+      document.body.style.backgroundColor = 'white'
+      showAlert('Light mode has been enabled','success')
     }
   }
   return (
     <>
       {/* <Navbar/> */}
       <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3" >
-        <TextForm heading="Enter your text to analyze below" mode={mode}/>
+        <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode} />
       </div>
     </>
   );
--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -5,19 +5,22 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked")
         let newText= text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to uppercase!","success")
     }
     const handleLoClick= ()=>{
         // console.log("Uppercase was clicked")
         let newText= text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to lowercase!","success")
     }
     const handleOnChange= (event)=>{
         // console.log("On Change");
         setText(event.target.value)
     }
-
+    
     const handleAltClick = () =>{       
         setText(text.split("").map(item => item === item.toUpperCase()? item.toLowerCase(): item.toUpperCase()).join(''))
+        props.showAlert("Converted to Alternative Case!","success")
     }
 
     const [text, setText] = useState("Enter text here")
@@ -38,7 +41,7 @@ export default function TextForm(props) {
 
         <div className="classname my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} words and {text.length} Characters</p>
+            <p>{text.trim().length>0?text.trim().split(/\s+/).length:0} words and {text.length} Characters</p>
             <p>{0.008*text.split(" ").length} Minutes Read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox above"}</p>
